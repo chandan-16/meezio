9bf7c0a1594c445ce12fffda1879ddec85a04027
chandan-16 <chandangautam420.cg@gmail.com>
2025-08-08 16:10:45 +0530

    Added new Assets to the projects & created route path for different routes and also created sidebar and navbar components of the app

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Meeting = ({params}: {params: {id: string}}) => {
   return (
-    <div>Meeting Room : #{params.id}</div>
+    <div className='bg-red-500'>Meeting Room : #{params.id}</div>
   )
 }
 
---
+++ components/ui/Sidebar.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import React from 'react'
+
+import { sidebarLinks } from '@/constants';
+import { usePathname } from 'next/navigation';
+
+import Image from 'next/image';
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+
+const Sidebar = () => {
+
+    const pathname = usePathname();
+
+  return (
+    <section className='sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-blue-700 p-6pt-28 text-white max-sm:hidden lg:w-[264px]'>
+        <div className='flex flex-col gap-2 mt-30 ml-5'>
+            {sidebarLinks.map((link) => {
+                const isActive = pathname === link.route || pathname.startsWith(link.route);
+                return(
+                    <Link href={link.route} key={link.label} className={cn('flex gap-4 w-[220px] items-center p-4 rounded-lg justify-start', {'bg-blue-600': isActive})} >
+                        <Image src={link.imgUrl} alt={link.label} width={24} height={24} />
+                        <p className='text-lg font-semibold max-lg:hidden'>
+                            {link.label}
+                        </p>
+                    </Link>
+                )
+            })}
+        </div>
+    </section>
+  )
+}
+
+export default Sidebar;

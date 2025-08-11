35eaaaad5ab92ac04eeb56caa968cbe4c6443b95
chandan-16 <chandangautam420.cg@gmail.com>
2025-08-11 21:39:43 +0530

    Added Cler Authentication to the App

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import MobileNav from './MobileNav';
+import { SignedIn, UserButton } from '@clerk/nextjs';
 
 const Navbar = () => {
   return (
@@ -10,8 +11,11 @@ const Navbar = () => {
             <Image src='/icons/logo.svg'  alt='Meezio Logo' width={32} height={32} className='max-sm:size-10'/>
             <p className='text-[26px] font-extrabold text-white max-sm:hidden'>Meezio</p>
         </Link>
-        <div className=' gap-5 '>
+        <div className='flex-between gap-5 '>
             {/* Cler USER mANAGEMENT */}
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
             <MobileNav />
         </div>
     </nav>

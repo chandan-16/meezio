f027b0f5f024de7d3a38baff2c8bf60a29ec7848
chandan-16 <chandangautam420.cg@gmail.com>
2025-08-13 12:54:10 +0530

    Initialized tailwindcss congi file and re-write the shadcn code and updated global css file

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,22 +24,22 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{ layout: { logoImageUrl : '/icons/yoom-logo.svg', socialButtonsVariant : 'iconButton' }, variables: {colorText : '#fff', colorPrimary : '#0E78F9', colorBackground : '#1c1f2e', colorInputBackground : '#252a41', colorInput : '#ffff' } }}>
       <html lang="en">
-        <body className={`${inter.className} bg-gray-950`}>
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <SignedOut>
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header>
+        <body style={{backgroundColor : '#000', color : 'white'}} className={`${inter.className} bg-gray-950`}>
+            {/* <header className="flex justify-end items-center p-4 gap-4 h-16">
+              <SignedOut>
+                <SignInButton />
+                <SignUpButton>
+                  <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                    Sign Up
+                  </button>
+                </SignUpButton>
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </header> */}
           {children}
         </body>
       </html>
